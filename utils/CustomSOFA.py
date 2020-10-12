53059d228cc5b6e6f11039729ea047a4ebfd9b9d
Evan Bubniak <evan.bubniak@gmail.com>
2020-10-12 14:00:16 -0400

    added conversion status methods

--- utils/CustomSOFA.py
+++ utils/CustomSOFA.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class SOFA:
     def __init__(self, obj):
 
@@ -41,3 +43,25 @@ class SOFA:
             "PositionType": obj.getEmitterPositionInfo()[1],
             "PositionUnits": obj.getEmitterPositionInfo()[0],
         }
+
+    @staticmethod
+    def sph2cart(coords):
+        alpha = coords[:, 0]*(np.pi/180)
+        beta = coords[:, 1]*(np.pi/180)
+        r = coords[:, 2]
+
+        x = r * np.cos(alpha) * np.cos(beta)
+        y = r * np.sin(alpha) * np.cos(beta)
+        z = r * np.sin(beta)
+        return np.array([x, y, z]).T
+
+    @staticmethod
+    def cart2sph(coords):
+        x = coords[:, 0]
+        y = coords[:, 1]
+        z = coords[:, 2]
+        
+        r = np.sqrt(x ** 2 + y ** 2 + z ** 2)
+        alpha = np.arctan2(y, x)
+        beta = np.arcsin(z / np.where(r != 0, r, 1))
+        return alpha, beta, r
