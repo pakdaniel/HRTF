e78b08f4577ce30e5c04dc50f09d4a30400d6a62
Evan Bubniak <evan.bubniak@gmail.com>
2020-10-22 21:33:43 -0400

    fixed excitation and sweep generation

--- acousticslib/generate_excitation.py
+++ acousticslib/generate_excitation.py
@@ -5,16 +5,16 @@ from math import ceil
 from acousticslib.domain_conversions import linearspectrum2timeseries
 
 def generate_excitation(record_length, pulse_time, num_records, excitation_type, sampling_frequency,
-    SineFrequency = 0,
-    SweepFrequencyHigh = 0,
-    SweepFrequencyLow = 0):
+    sin_freq = 0,
+    sweep_freq_high = 0,
+    sweep_freq_low = 0):
 
     # Set random seed for reproducibility and debugging
     np.random.seed(31415)
     record_length = int(record_length)
 
-    f2 = SweepFrequencyHigh
-    f1 = SweepFrequencyLow
+    f2 = sweep_freq_high
+    f1 = sweep_freq_low
 
     dt = 1/sampling_frequency
 
@@ -24,11 +24,11 @@ def generate_excitation(record_length, pulse_time, num_records, excitation_type,
     if excitation_type =='impulse':
         
         pulse_length = 1
-        pulse = 1
+        pulse = [1]
 
     elif excitation_type == 'cw_pulse':
         # if no explicitly given sine frequency, use one full pulse.
-        sine_freq = SineFrequency
+        sine_freq = sin_freq
         if not sine_freq:
             sine_freq = 1/pulse_time 
         
@@ -45,12 +45,12 @@ def generate_excitation(record_length, pulse_time, num_records, excitation_type,
         pulse = np.sin(log_sine_sweep(pulse_t_range, pulse_time, f1, f2))
 
     elif excitation_type== 'white_noise':
-        X = np.ones((1, pulse_length))
+        X = np.ones((pulse_length))
 
-        phase_noise = (2*np.pi)*np.uniform((1,ceil(pulse_length/2 - 1)))
+        phase_noise = (2*np.pi)*np.random.uniform(size=(ceil(pulse_length/2 - 1)))
 
-        X[:len(phase_noise)] = np.exp(1j*phase_noise)
-        X[ ceil(pulse_length/2 + 1) - 1 : pulse_length - ((pulse_length % 2) == 0)] = np.conj(np.exp(1j*np.flip(phase_noise)))
+        X[:phase_noise.shape[0]] = np.exp(1j*phase_noise)
+        X[ceil(pulse_length/2 + 1) - 1 : pulse_length - ((pulse_length % 2) == 0)] = np.conj(np.exp(1j*np.flip(phase_noise)))
         pulse, _ = linearspectrum2timeseries(X, dt)    
 
     elif excitation_type== 'pink_noise':
@@ -60,12 +60,12 @@ def generate_excitation(record_length, pulse_time, num_records, excitation_type,
         X = np.sqrt(np.divide(1, np.abs(shifted_frequency_range)))
         inf_index = ceil(pulse_length/2) - 1
         X[inf_index] = 1
-        phase_noise = (2*np.pi)*np.uniform((1,ceil(pulse_length/2 - 1)))
+        phase_noise = (2*np.pi)*np.random.uniform(size=(ceil(pulse_length/2 - 1)))
         X[:len(phase_noise)] = X[:len(phase_noise)]*np.exp(1j*phase_noise)
         X[ceil(pulse_length/2 + 1) - 1: pulse_length - ((pulse_length % 2) == 0)] = np.multiply(X[ ceil(pulse_length/2 + 1) - 1 : pulse_length - ((pulse_length % 2) == 0)], np.conj(np.exp(1j*np.flip(phase_noise))))
 
         pulse, _ = linearspectrum2timeseries(X, dt)    
        
-    single_record = np.concatenate((pulse, np.zeros((record_length - pulse_length))))
+    single_record = np.concatenate((np.atleast_2d(pulse), np.atleast_2d(np.zeros((record_length - pulse_length)))), axis=1)
     excitation = repmat(single_record, 1, num_records)
     return excitation, t_range
--- acousticslib/sweeps.py
+++ acousticslib/sweeps.py
@@ -3,4 +3,4 @@ def lin_sine_sweep(t, T_p, f1, f2):
     return 2*pi*( (((f2 - f1)/(2*T_p)) * square(t)) + f1*t)
 
 def log_sine_sweep(t, T_p, f1, f2):
-    return 2*pi*(( f1*T_p* power((f2/f1)^(t/T_p)))/(log(f2/f1)) - (f1*T_p)/(log(f2/f1)))
+    return 2*pi*(( f1*T_p* power((f2/f1),(t/T_p)))/(log(f2/f1)) - (f1*T_p)/(log(f2/f1)))
