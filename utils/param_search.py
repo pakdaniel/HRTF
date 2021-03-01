9e6ef9201b5a183d327910c51ac012799aefce16
Evan Bubniak <evan.bubniak@gmail.com>
2021-03-01 10:38:41 -0500

    improve formatting

--- utils/param_search.py
+++ utils/param_search.py
@@ -28,7 +28,7 @@ def grid_search(model = None, hrir_all = None, verbose=0, num_epochs = 100):
             best_loss = loss
             best_pair = pair
         # loss = model.log.history['loss'][-1]
-        print("{}, {}: {:.4f}; Holdout: {}; current best is {} with {:.4f}".format(pair[0], pair[1], loss, holdout_num, best_pair, best_loss))
+        print("{}, {}: {:.4f}; Holdout: {}; current best is [({},{}), ({}, {})] with {:.4f}".format(pair[0], pair[1], loss, holdout_num, best_pair[0][0], best_pair[0][1], best_pair[1][0], best_pair[1][1], best_loss))
 
         pairs_losses.append((pair, loss))
     
