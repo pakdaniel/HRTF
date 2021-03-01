aed9a224ed7d0444d05edf5360e567069f433599
Evan Bubniak <evan.bubniak@gmail.com>
2021-03-01 12:49:47 -0500

    add progress bar and enumerate iterations in print

--- utils/param_search.py
+++ utils/param_search.py
@@ -3,19 +3,24 @@ from .split_dataset import split_dataset
 from itertools import combinations
 from numpy import sum
 
-def get_all_pairs(positions):
-    return combinations(positions[:, :2], 2)
+def get_all_combs(positions, num):
+    return combinations(positions[:, :2], num)
 
 
-def grid_search(model = None, hrir_all = None, verbose=0, num_epochs = 100):
-    pairs = get_all_pairs(hrir_all[0].Source["Position"])
+def grid_search(model = None, hrir_all = None, verbose=0, num_epochs = 100, num_positions = 2, notebook = False):
+    pairs = get_all_combs(hrir_all[0].Source["Position"], num_positions)
     callback = EarlyStopping(monitor='loss', patience=3)
     best_pair = None
     best_loss = None
     pairs_losses = []
     model.compile(optimizer="adam")
     weights = model.model.get_weights()
-    for pair in pairs:
+    if notebook:
+        from tqdm.notebook import tqdm
+    else:
+        from tqdm import tqdm
+    
+    for i, pair in tqdm(enumerate(pairs), total=len(pairs)):
         X_train, y_train, X_holdout, y_holdout, X_test, y_test, holdout_num = split_dataset(hrir_all, observer_of_interest = 0, positions_of_interest = pair, channel = "left")
         model.model.set_weights(weights)
         model.fit(X_train,y_train,X_test,y_test, verbose=verbose, num_epochs = num_epochs, save_weights=False, callbacks=callback)
@@ -28,7 +33,16 @@ def grid_search(model = None, hrir_all = None, verbose=0, num_epochs = 100):
             best_loss = loss
             best_pair = pair
         # loss = model.log.history['loss'][-1]
-        print("{}, {}: {:.4f}; Holdout: {}; current best is [({},{}), ({}, {})] with {:.4f}".format(pair[0], pair[1], loss, holdout_num, int(best_pair[0][0]), int(best_pair[0][1]), int(best_pair[1][0]), int(best_pair[1][1]), best_loss))
+        print("{}/{}: {}: {:.4f}; Holdout: {}; current best is {} with {:.4f}".format(
+            i,
+            len(pairs),
+            "[" + ", ".join(["({}, {})".format(item[0], item[1]) for item in pair]) + "]",
+            loss,
+            holdout_num,
+            "[" + ", ".join(["({}, {})".format(item[0], item[1]) for item in best_pair]) + "]",
+            best_loss
+            )
+        )
 
         pairs_losses.append((pair, loss))
     
