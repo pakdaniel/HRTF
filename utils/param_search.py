f581973b18b26986760a7cab45ac03b9295b9436
Evan Bubniak <evan.bubniak@gmail.com>
2021-03-01 12:50:51 -0500

    replace pair with coord set for clarity if more than 2 points used

--- utils/param_search.py
+++ utils/param_search.py
@@ -8,11 +8,11 @@ def get_all_combs(positions, num):
 
 
 def grid_search(model = None, hrir_all = None, verbose=0, num_epochs = 100, num_positions = 2, notebook = False):
-    pairs = get_all_combs(hrir_all[0].Source["Position"], num_positions)
+    coord_sets = get_all_combs(hrir_all[0].Source["Position"], num_positions)
     callback = EarlyStopping(monitor='loss', patience=3)
-    best_pair = None
+    best_coord_set = None
     best_loss = None
-    pairs_losses = []
+    coord_sets_losses = []
     model.compile(optimizer="adam")
     weights = model.model.get_weights()
     if notebook:
@@ -20,31 +20,31 @@ def grid_search(model = None, hrir_all = None, verbose=0, num_epochs = 100, num_
     else:
         from tqdm import tqdm
     
-    for i, pair in tqdm(enumerate(pairs), total=len(pairs)):
-        X_train, y_train, X_holdout, y_holdout, X_test, y_test, holdout_num = split_dataset(hrir_all, observer_of_interest = 0, positions_of_interest = pair, channel = "left")
+    for i, coord_set in tqdm(enumerate(coord_sets), total=len(coord_sets)):
+        X_train, y_train, X_holdout, y_holdout, X_test, y_test, holdout_num = split_dataset(hrir_all, observer_of_interest = 0, positions_of_interest = coord_set, channel = "left")
         model.model.set_weights(weights)
         model.fit(X_train,y_train,X_test,y_test, verbose=verbose, num_epochs = num_epochs, save_weights=False, callbacks=callback)
         y_predict = model.predict(X_holdout)
         loss = (sum(y_predict - y_holdout)**2)/len(y_predict)
         if not best_loss:
             best_loss = loss
-            best_pair = pair
+            best_coord_set = coord_set
         elif loss < best_loss:
             best_loss = loss
-            best_pair = pair
+            best_coord_set = coord_set
         # loss = model.log.history['loss'][-1]
         print("{}/{}: {}: {:.4f}; Holdout: {}; current best is {} with {:.4f}".format(
             i,
-            len(pairs),
-            "[" + ", ".join(["({}, {})".format(item[0], item[1]) for item in pair]) + "]",
+            len(coord_sets),
+            "[" + ", ".join(["({}, {})".format(item[0], item[1]) for item in coord_set]) + "]",
             loss,
             holdout_num,
-            "[" + ", ".join(["({}, {})".format(item[0], item[1]) for item in best_pair]) + "]",
+            "[" + ", ".join(["({}, {})".format(item[0], item[1]) for item in best_coord_set]) + "]",
             best_loss
             )
         )
 
-        pairs_losses.append((pair, loss))
+        coord_sets_losses.append((coord_set, loss))
     
-    print(f"Best pair is {best_pair} with {best_loss} loss")
-    return pairs_losses
+    print(f"Best coordinate set is {best_coord_set} with {best_loss} loss")
+    return coord_sets_losses
