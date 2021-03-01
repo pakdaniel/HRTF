2cf36007ca89fa22746f355d205789b1c118ba2b
Evan Bubniak <evan.bubniak@gmail.com>
2021-03-01 10:36:35 -0500

    return pairs from gridsearch

--- utils/param_search.py
+++ utils/param_search.py
@@ -21,15 +21,16 @@ def grid_search(model = None, hrir_all = None, verbose=0, num_epochs = 100):
         model.fit(X_train,y_train,X_test,y_test, verbose=verbose, num_epochs = num_epochs, save_weights=False, callbacks=callback)
         y_predict = model.predict(X_holdout)
         loss = (sum(y_predict - y_holdout)**2)/len(y_predict)
-        # loss = model.log.history['loss'][-1]
-        print("{}, {}: {:.4f}; Holdout: {}; current best is {} with {:.4f}".format(pair[0], pair[1], loss, holdout_num, best_pair, best_loss))
         if not best_loss:
             best_loss = loss
             best_pair = pair
         elif loss < best_loss:
             best_loss = loss
             best_pair = pair
+        # loss = model.log.history['loss'][-1]
+        print("{}, {}: {:.4f}; Holdout: {}; current best is {} with {:.4f}".format(pair[0], pair[1], loss, holdout_num, best_pair, best_loss))
+
         pairs_losses.append((pair, loss))
     
     print(f"Best pair is {best_pair} with {best_loss} loss")
-    
+    return pairs_losses
